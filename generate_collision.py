a0fe3a35d2785722df525785f0d73274457bddc0
Matthew Wang <107924410+matthew-mw@users.noreply.github.com>
2024-11-23 13:45:37 -0500

    added paths

--- generate_collision.py
+++ generate_collision.py
@@ -6,9 +6,8 @@ from svgpathtools import svg2paths
 svg_file = "./ASSETS/Trans-Dark.svg"
 paths, attributes = svg2paths(svg_file)
 
-
 # Function to sample points along a path
-def sample_points(path, num_points=200):
+def sample_points(path, num_points=1000):
     total_length = path.length()
     sampled_points = []
     for i in range(num_points):
@@ -21,24 +20,5 @@ def sample_points(path, num_points=200):
 # Extract points from all paths
 all_points = []
 for path in paths:
-    points = sample_points(path, num_points=50)  # Sample from each path
-    all_points.extend(points)
-
-# If more points are needed, adjust accordingly
-num_coordinates = 200
-if len(all_points) > num_coordinates:
-    sampled_coordinates = all_points[:num_coordinates]
-else:
-    sampled_coordinates = all_points
-
-sampled_coordinates = sampled_coordinates[0:50]
-print("")
-print("")
-print("")
-
-all_coords = []
-for i in sampled_coordinates:
-    all_coords.append(i[0])
-    all_coords.append(i[1])
-
-print(all_coords)
+    points = sample_points(path)
+    print(", ".join(f"{p[0]-158}, {p[1]-457.5}" for p in points))
